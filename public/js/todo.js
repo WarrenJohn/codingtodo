d4e18d9d3cdc2f61230c3d73e8e67cd1f9ba6cd1
Warren <warrenjohn89@gmail.com>
2019-05-29 14:47:28 -0400

    added css style, changed page layout slighty

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,5 +1,6 @@
 (function(){
-    const todo = document.getElementById('todo');
+
+    const todoInput = document.getElementById('todo');
     const list = document.getElementById('list');
     const li = list.querySelectorAll('li');
     const date = new Date();
@@ -49,35 +50,59 @@
         return li;
     }
 
-    function addTodo (userInput){
-        let ranID = Math.random().toString(36).substring(7);
-        const li = document.createElement('li');
-        const label = document.createElement('label');
-        const input = document.createElement('input');
-        const span = document.createElement('span');
-        label.id = ranID;
-        label.className += 'check-container';
-        input.type = 'checkbox'
-        span.className += 'checkmark';
+    // function addTodo (userInput){
+    //     let randomID = Math.random().toString(36).substring(7);
+    //     const li = document.createElement('li');
+    //     const label = document.createElement('label');
+    //     const input = document.createElement('input');
+    //     const span = document.createElement('span');
+    //     label.id = randomID;
+    //     label.className += 'check-container';
+    //     input.type = 'checkbox'
+    //     span.className += 'checkmark';
+    //
+    //     label.textContent = userInput + ` (${new Date().toDateString()})`;
+    //
+    //     li.appendChild(label);
+    //     label.appendChild(input);
+    //     label.appendChild(span)
+    //     return li;
+    // }
 
-        label.textContent = userInput + ` (${new Date().toDateString()})`;
+    function Todo (){
+        this.randomID = Math.random().toString(36).substring(7);
+        this.li = document.createElement('li');
+        this.label = document.createElement('label');
+        this.input = document.createElement('input');
+        this.span = document.createElement('span');
 
-        li.appendChild(label);
-        label.appendChild(input);
-        label.appendChild(span)
-        return li;
+        this.add = function(userInput){
+            this.label.id = this.randomID;
+            this.label.className += 'check-container';
+            this.input.type = 'checkbox'
+            this.span.className += 'checkmark';
+
+            this.label.textContent = userInput + ` (${new Date().toDateString()})`;
+
+            this.li.appendChild(this.label);
+            this.label.appendChild(this.input);
+            this.label.appendChild(this.span)
+            return this.li;
+        }
+
     }
 
-    existingTodo ? existingTodo.forEach(item => {
-        list.appendChild(setUp(item.id, item.text, item.checked))
+    existingTodo ? existingTodo.forEach(todo => {
+        list.appendChild(setUp(todo.id, todo.text, todo.checked))
     }) : false;
 
-    todo.onkeydown = (e) => {
-        if (e.keyCode == 13) {
-            const item = addTodo(todo.value);
-            list.appendChild(item);
+    todoInput.onkeydown = (e) => {
+        if (e.keyCode == 13) { // enter key is 13
+            // const todo = addTodo(todo.value);
+            const todo = new Todo();
+            list.appendChild(todo.add(todoInput.value));
             addItem()
-            todo.value = '';
+            todoInput.value = ''; // input field value
         }
     }
     list.addEventListener('click', e => {
